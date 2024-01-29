75cf638eaf43ddbf0017d77edfcc8fa6aa8be527
JC <jeanchristophe.nicolas@vodafone.com>
2024-01-29 15:11:25 +0000

    add global declaration

---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+declare module 'UI/Button' 
+// {
+//     export interface ButtonProps {
+//         onClick?: (event: MouseEvent<HTMLButtonElement>) => void
+//         label: string
+//         type?: 'button' | 'submit'
+//         disabled?: boolean
+//     }
+// }
